a95f36d770c31f3436ec7c959dace14a4212015d
Mihai Galos <mihaigalos@gmail.com>
2020-04-22 18:45:14 +0200

    Test TRx

--- test/unit_logic.cpp
+++ test/unit_logic.cpp
@@ -11,24 +11,45 @@ using ::testing::AtLeast;
 
 class Fixture : public ::testing::Test
 {
+public:
+  static void generic_transmit_byte(const uint8_t &payload)
+  {
+    data_ = payload;
+  }
+
+  static uint8_t generic_receive_byte()
+  {
+    return static_cast<uint8_t>(data_);
+  }
+
 protected:
-  virtual void SetUp() override {}
+  virtual void SetUp() override { data_ = {}; }
   virtual void TearDown() override {}
 
-  auto MakeGenericHandshake()
-  {
-    auto generic_transmit_byte = [](const uint8_t &payload) {};
-    auto generic_receive_byte = []() { return static_cast<uint8_t>('A'); };
-    return UartHanshake{generic_transmit_byte, generic_receive_byte};
-  }
+  static uint8_t data_;
 };
 
-TEST_F(Fixture, TransmitCalled_WhenTypical)
+uint8_t Fixture::data_;
+
+TEST_F(Fixture, ReceiveCalled_WhenTypical)
 {
-  auto sut_ = MakeGenericHandshake();
+  auto sut_ = UartHanshake{Fixture::generic_transmit_byte, Fixture::generic_receive_byte};
   uint8_t expected_byte = 'A';
+  data_ = expected_byte;
 
   auto received_byte = sut_.Receive();
 
   ASSERT_EQ(expected_byte, received_byte);
+}
+
+TEST_F(Fixture, TransmitCalled_WhenTypical)
+{
+  uint8_t transmitted_byte = {};
+  auto sut_ = UartHanshake{generic_transmit_byte, generic_receive_byte};
+  uint8_t byte_to_transmit = 'A';
+
+  sut_.Transmit(byte_to_transmit);
+  transmitted_byte = data_;
+
+  ASSERT_EQ(transmitted_byte, byte_to_transmit);
 }
