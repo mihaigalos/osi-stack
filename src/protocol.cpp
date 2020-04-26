f64c5dc9cb982845f0b4781aa329fbb878753285
Mihai Galos <mihaigalos@gmail.com>
2020-04-26 21:25:31 +0200

    Eliminate switch-case

--- src/protocol.cpp
+++ src/protocol.cpp
@@ -6,10 +6,9 @@ template <>
 CommunicationStatus UartHandshake<>::RetransmitWithAcknowledge(const Payload &payload, uint8_t retransmit_count)
 {
     CommunicationStatus result{CommunicationStatus::Unknown};
-    for (uint8_t i = 0; i < retransmit_count; ++i)
+    for (uint8_t i = 0; i < retransmit_count && result != CommunicationStatus::Acknowledge; ++i)
     {
         io_.Transmit(payload);
-
         Payload response = io_.Receive();
 
         log_dump_payload(response, "response");
@@ -17,17 +16,7 @@ CommunicationStatus UartHandshake<>::RetransmitWithAcknowledge(const Payload &pa
         if (response.size)
         {
             result = static_cast<CommunicationStatus>(response.data[0]);
-            switch (result)
-            {
-            case CommunicationStatus::NegativeAcknowledge:
-                log("Received negative response, retransmitting.");
-                break;
-            case CommunicationStatus::Acknowledge:
-                log("Received acknowledge response, everything OK.");
-                return CommunicationStatus::Acknowledge;
-            default:
-                break;
-            }
+            log("Received CommunicationStatus response: " + std::to_string(response.data[0]));
         }
     }
     return result;
