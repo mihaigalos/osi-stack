4e81496c2281c0308e5ff9356e4ee0c064a48516
Mihai Galos <mihaigalos@gmail.com>
2020-04-27 17:24:07 +0200

    Constify TransmitWithAcknowledge

--- src/protocol.cpp
+++ src/protocol.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 template <>
-CommunicationStatus UartHandshake<>::TransmitWithAcknowledge(const Payload &payload, uint8_t retransmit_count)
+CommunicationStatus UartHandshake<>::TransmitWithAcknowledge(const Payload &payload, uint8_t retransmit_count) const
 {
     CommunicationStatus result{CommunicationStatus::Unknown};
     for (uint8_t i = 0; i < retransmit_count && result != CommunicationStatus::Acknowledge; ++i)
