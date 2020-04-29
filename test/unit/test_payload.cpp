8a8db84b345f1143918bac3acd8a3f82dbc4527f
Mihai Galos <mihaigalos@gmail.com>
2020-04-29 17:42:03 +0200

    Add u-tests for Payload

---
+++ test/unit/test_payload.cpp
@@ -0,0 +1,52 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <memory>
+#include <string>
+
+#include "payload.h"
+
+using ::testing::AtLeast;
+
+class Fixture : public ::testing::Test
+{
+public:
+protected:
+    virtual void SetUp() override
+    {
+        payloadified_ = Payload{data_.c_str(), static_cast<uint8_t>(data_.length())};
+    }
+    virtual void TearDown() override {}
+
+    static std::string data_;
+    static Payload payloadified_;
+};
+
+std::string Fixture::data_{"abcd"};
+Payload Fixture::payloadified_;
+
+TEST_F(Fixture, PayloadAssignmentWorks_WhenTypical)
+{
+    Payload actual{payloadified_};
+
+    Payload expected = actual;
+
+    ASSERT_EQ(expected, actual);
+}
+
+TEST_F(Fixture, PayloadConstructionFromConstCharWorks_WhenTypical)
+{
+    std::string expected{"abcd"};
+
+    bool is_match = true;
+    for (uint8_t i = 0; i < payloadified_.size; ++i)
+    {
+        if (expected[i] != payloadified_.data[i])
+        {
+            is_match = false;
+            break;
+        }
+    }
+
+    ASSERT_TRUE(is_match);
+}
