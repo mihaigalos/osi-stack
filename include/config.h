6aa00531f658825199e247528cb1183d805d1601
Mihai Galos <mihaigalos@gmail.com>
2020-04-29 17:48:04 +0200

    Unify syntax

--- include/config.h
+++ include/config.h
@@ -4,6 +4,6 @@
 
 using CRCChecksum = uint16_t;
 
-constexpr uint8_t kCRCSize = sizeof(CRCChecksum);
+constexpr uint8_t kCRCSize{sizeof(CRCChecksum)};
 constexpr uint8_t kMaxRetransmitCount{2};
 constexpr uint8_t kPayloadMaxSize{16};
