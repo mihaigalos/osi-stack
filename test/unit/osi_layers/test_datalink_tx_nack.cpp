55401a1d5de0072d13280f651141e9b7fc91e310
Mihai Galos <mihaigalos@gmail.com>
2020-05-01 23:13:37 +0200

    Green test_datalink_tx_nack

--- test/unit/osi_layers/test_datalink_tx_nack.cpp
+++ test/unit/osi_layers/test_datalink_tx_nack.cpp
@@ -11,6 +11,7 @@
 #include "utilities.h"
 
 constexpr uint8_t data_negative_acknowledge_[]{static_cast<uint8_t>(CommunicationStatus::NegativeAcknowledge)};
+constexpr uint8_t data_acknowledge_[]{static_cast<uint8_t>(CommunicationStatus::Acknowledge)};
 
 class Fixture : public ::testing::Test
 {
@@ -22,11 +23,22 @@ public:
 
     static uint8_t generic_receive_byte()
     {
-        if (received_.size == 3)
-        {
-            received_.size = 0;
-        }
-        return payloadified_negative_acknowledge_.data[received_.size++];
+        static uint8_t call_count = 0;
+        constexpr uint8_t payload_size_byte_count{1};
+
+        std::map<uint8_t, uint8_t> lookup_map{
+            {0, payload_size_byte_count + kCRCSize},
+            {1, payloadified_negative_acknowledge_.data[0]},
+            {2, payloadified_negative_acknowledge_.data[1]},
+            {3, payloadified_negative_acknowledge_.data[2]},
+
+            {4, payload_size_byte_count + kCRCSize},
+            {5, payloadified_negative_acknowledge_.data[0]},
+            {6, payloadified_negative_acknowledge_.data[1]},
+            {7, payloadified_negative_acknowledge_.data[2]},
+        };
+
+        return lookup_map[call_count++];
     }
 
 protected:
@@ -35,8 +47,10 @@ protected:
         received_.Reset();
 
         payloadified_negative_acknowledge_ = Payload{data_negative_acknowledge_, 1};
+        payloadified_acknowledge_ = Payload{data_acknowledge_, 1};
 
         payloadified_negative_acknowledge_ = append_crc_to_payload(payloadified_negative_acknowledge_);
+        payloadified_acknowledge_ = append_crc_to_payload(payloadified_acknowledge_);
 
         payloadified_data_ = Payload{data_.c_str(), static_cast<uint8_t>(data_.length())};
         received_.size = 0;
@@ -48,6 +62,7 @@ protected:
 
     static std::string data_;
     static Payload payloadified_negative_acknowledge_;
+    static Payload payloadified_acknowledge_;
     static Payload payloadified_data_;
 };
 
@@ -55,9 +70,10 @@ Payload Fixture::received_{};
 
 std::string Fixture::data_{"abcd"};
 Payload Fixture::payloadified_negative_acknowledge_;
+Payload Fixture::payloadified_acknowledge_;
 Payload Fixture::payloadified_data_;
 
-TEST_F(Fixture, TransmitNegativeAcknowledgeWorks_WhenTypical)
+TEST_F(Fixture, TransmitWithAcknowledgeWorks_WhenTypical)
 {
     CommunicationStatus result = sut_.TransmitWithAcknowledge(payloadified_data_);
 
