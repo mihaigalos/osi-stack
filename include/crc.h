226d43988993b8a3a0c39ffb497dde7129e756e7
Mihai Galos <mihaigalos@gmail.com>
2020-05-02 12:27:51 +0200

    Apply rule of 5 for CRC

--- include/crc.h
+++ include/crc.h
@@ -5,6 +5,14 @@
 class CRC
 {
 public:
+    CRC() = default;
+    virtual ~CRC() = default;
+
+    CRC(const CRC &other) = delete;
+    CRC(CRC &&other) = delete;
+    CRC &operator=(const CRC &other) = delete;
+    CRC &operator=(CRC &&other) = delete;
+
     Payload append_crc_to_payload(const Payload &payload) const;
     CRCChecksum computed_crc(const Payload &payload, uint8_t bytes_count) const;
     CRCChecksum received_crc(const Payload &payload) const;
