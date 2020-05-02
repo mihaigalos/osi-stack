cdd37fc1b84b105c78fdcb281dbbab7979786710
Mihai Galos <mihaigalos@gmail.com>
2020-05-02 20:37:49 +0200

    Simplify Tx

--- src/osi_layers/network.cpp
+++ src/osi_layers/network.cpp
@@ -6,22 +6,10 @@
 template <>
 CommunicationStatus Network<>::TransmitTo(Payload &payload, uint8_t to) const
 {
-    CommunicationStatus result{CommunicationStatus::Unknown};
-
-    if (payload.size + sizeof(to) + sizeof(own_id_) < kPayloadMaxSize)
-    {
-        payload.data[payload.size++] = to;
-        payload.data[payload.size++] = own_id_;
-        log_dump_payload(payload, "Network :: TransmitTo");
-        return datalink_.TransmitWithAcknowledge(payload);
-    }
-    else
-    {
-        log("Size of payload > maximum allowed size");
-        log_dump_payload(payload, "Network :: TransmitTo");
-    }
-
-    return result;
+    payload.data[payload.size++] = to;
+    payload.data[payload.size++] = own_id_;
+    log_dump_payload(payload, "Network :: TransmitTo");
+    return datalink_.TransmitWithAcknowledge(payload);
 }
 
 template <>
