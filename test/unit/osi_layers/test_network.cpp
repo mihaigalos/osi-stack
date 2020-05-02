14c07e863d11d2ede2e8074c0f56d86b24ba9793
Mihai Galos <mihaigalos@gmail.com>
2020-05-02 20:21:00 +0200

    Only ACK when ownid matches to_id

--- test/unit/osi_layers/test_network.cpp
+++ test/unit/osi_layers/test_network.cpp
@@ -14,17 +14,28 @@
 constexpr uint8_t kOwnId{0x01};
 constexpr uint8_t kDestinationId{0x02};
 
+constexpr uint8_t kPosDestinationIdInPayload{6};
+constexpr uint8_t kPosSourceIdInPayload{7};
+
 class Fixture : public UnitBase
 {
 public:
     static void generic_transmit_byte(const uint8_t payload)
     {
+        if (transmitted_.size == kPosDestinationIdInPayload && payload == kOwnId)
+        {
+            to_field_matches_own_ = true;
+        }
         transmitted_.data[transmitted_.size++] = payload;
     }
 
     static uint8_t generic_receive_byte()
     {
-        return lookup_map_[call_count_++];
+        if (to_field_matches_own_)
+        {
+            return lookup_map_[call_count_++];
+        }
+        return 0xFF;
     }
 
 protected:
@@ -33,38 +44,39 @@ protected:
 
         UnitBase::SetUp();
         call_count_ = 0;
+        to_field_matches_own_ = false;
         lookup_map_ = {
             {0, payload_size_byte_count + kCRCSize},
-            {1, payloadified_negative_acknowledge_.data[0]},
-            {2, payloadified_negative_acknowledge_.data[1]},
-            {3, payloadified_negative_acknowledge_.data[2]},
+            {1, payloadified_acknowledge_.data[0]},
+            {2, payloadified_acknowledge_.data[1]},
+            {3, payloadified_acknowledge_.data[2]},
 
             {4, payload_size_byte_count + kCRCSize},
-            {5, payloadified_acknowledge_.data[0]},
-            {6, payloadified_acknowledge_.data[1]},
-            {7, payloadified_acknowledge_.data[2]},
+            {5, payloadified_negative_acknowledge_.data[0]},
+            {6, payloadified_negative_acknowledge_.data[1]},
+            {7, payloadified_negative_acknowledge_.data[2]},
         };
     }
     Network<> sut_{kOwnId, Datalink<>{Physical{generic_transmit_byte, generic_receive_byte}}};
     static uint8_t call_count_;
+    static bool to_field_matches_own_;
 };
 
 uint8_t Fixture::call_count_;
+bool Fixture::to_field_matches_own_;
+
+TEST_F(Fixture, NetworkTransmitToWorks_WhenTypical)
+{
+
+    sut_.TransmitTo(payloadified_data_, kDestinationId);
+
+    ASSERT_EQ(transmitted_.data[kPosDestinationIdInPayload - 1], kDestinationId);
+    ASSERT_EQ(transmitted_.data[kPosSourceIdInPayload - 1], kOwnId);
+}
 
 TEST_F(Fixture, NetworkTransmitToResultAcknowledge_WhenTypical)
 {
     CommunicationStatus result = sut_.TransmitTo(payloadified_data_, kDestinationId);
 
     ASSERT_EQ(result, CommunicationStatus::Acknowledge);
-}
-
-TEST_F(Fixture, NetworkTransmitToWorks_WhenTypical)
-{
-    uint8_t pos_destination_id_in_payload{5};
-    uint8_t pos_source_id_in_payload{6};
-
-    sut_.TransmitTo(payloadified_data_, kDestinationId);
-
-    ASSERT_EQ(transmitted_.data[pos_destination_id_in_payload], kDestinationId);
-    ASSERT_EQ(transmitted_.data[pos_source_id_in_payload], kOwnId);
 }
