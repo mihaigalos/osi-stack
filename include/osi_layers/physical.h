47f62166243109297c00e870465424f1a62e9c15
Mihai Galos <mihaigalos@gmail.com>
2020-05-02 12:31:08 +0200

    Apply rule of 5 for Physical

--- include/osi_layers/physical.h
+++ include/osi_layers/physical.h
@@ -10,6 +10,13 @@ using TUint8Void = uint8_t (*)();
 class Physical
 {
 public:
+  virtual ~Physical() = default;
+
+  Physical(const Physical &other) = delete;
+  Physical(Physical &&other) = delete;
+  Physical &operator=(const Physical &other) = delete;
+  Physical &operator=(Physical &&other) = delete;
+
   Physical(TVoidUint8 on_transmit, TUint8Void on_receive) : on_transmit_byte_{on_transmit}, on_receive_byte_{on_receive} {}
 
   void Transmit(const Payload &payload) const;
