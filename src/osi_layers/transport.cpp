1198cbf7b6adb88e95aef8a7d7dadf06a53138dc
Mihai Galos <mihaigalos@gmail.com>
2020-05-04 22:27:05 +0200

    Flip segment from iterating up to iterating down
    The reason for this is the following:
    When iterating up, one starts at 0 and goes up to how many segments
    need to be sent until the full array of information is fully
    transmitted.
    
    The problem is on the receiving end, one cannot just call Receive()
    at the Transport layer, since one does not know the total number of
    segments. So this information either needs to somehow be transmitted,
    or a delegation to ReceiveNumberOfSegments needs to be asked from
    Transport to Network, to Datalink and finally to Physical.
    
    My iterating down, the receiver can continue calling the lower Netork
    Receive() and decrementing the segment index until it reaches zero.

--- src/osi_layers/transport.cpp
+++ src/osi_layers/transport.cpp
@@ -6,23 +6,30 @@ template <>
 CommunicationStatus Transport<>::Transmit(uint8_t to, uint8_t *data, uint32_t size) const
 {
     auto result{CommunicationStatus::Unknown};
-    TSegment segment{};
+
+    uint8_t payload_without_metadata{kPayloadMaxSize - kSizeOfToField - kSizeOfFromField - kCRCSize - sizeof(TSegment)};
+
+    uint16_t full_segments{static_cast<uint16_t>(size / payload_without_metadata)};
+    uint16_t partial_segments{static_cast<uint16_t>(size % payload_without_metadata)};
+    uint16_t make_last_segment_be_zero{1};
+    TSegment segment{static_cast<uint16_t>(full_segments + (partial_segments != 0 ? 1 : 0) - make_last_segment_be_zero)};
 
     for (uint32_t i = 0; i < size;)
     {
         uint8_t j{0};
         Payload payload{};
-        for (j = 0; j <= (kPayloadMaxSize - kSizeOfToField - kSizeOfFromField - kCRCSize - sizeof(TSegment)) && (i + j <= size); ++j)
+        for (j = 0; j <= (payload_without_metadata) && (i + j <= size); ++j)
         {
             payload.data[j] = data[i + j];
         }
         payload.size = j - 1;
+
         i += j - 1;
 
         payload.data[payload.size++] = static_cast<uint8_t>(segment);
         payload.data[payload.size++] = static_cast<uint8_t>(segment >> 8);
         log_dump_payload(payload, std::string{"Transport :: Transmit ["} + std::to_string(segment) + "]");
-        ++segment;
+        --segment;
 
         result = network_.Transmit(to, payload);
 
