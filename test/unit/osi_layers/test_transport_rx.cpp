4b357ac449f90d2671a3c9f7a4c655829f2ef400
Mihai Galos <mihaigalos@gmail.com>
2020-05-05 21:26:57 +0200

    Intermediate state test

---
+++ test/unit/osi_layers/test_transport_rx.cpp
@@ -0,0 +1,50 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <memory>
+#include <string>
+
+#include "osi_layers/physical.h"
+#include "crc.h"
+#include "osi_layers/transport.h"
+
+#include "utilities.h"
+#include "test_unit_base.h"
+
+class Fixture : public UnitBase
+{
+public:
+    static void generic_transmit_byte(const uint8_t payload)
+    {
+        static_cast<void>(payload);
+    }
+
+    static uint8_t generic_receive_byte()
+    {
+        static uint8_t call_count{0};
+
+        if (call_count++ > 0)
+        {
+            return payloadified_data_with_segment_to_from_crc_.data[call_count - kSizeofLength - 1];
+        }
+        else
+        {
+            return payloadified_data_with_segment_to_from_crc_.size;
+        }
+    }
+
+protected:
+    virtual void SetUp() override
+    {
+        UnitBase::SetUp();
+    }
+    Transport<> sut_{Network<>{kOwnId, {Datalink<>{Physical{generic_transmit_byte, generic_receive_byte}}}}};
+    uint8_t buffer_[1024];
+};
+
+TEST_F(Fixture, ReceiveWorks_WhenTypical)
+{
+    auto actual = sut_.Receive(kSourceId, &buffer_[0]);
+
+    ASSERT_NE(actual, 0);
+}
