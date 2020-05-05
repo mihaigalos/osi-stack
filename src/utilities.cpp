07406907cc22ce2c86903169f44366465f655457
Mihai Galos <mihaigalos@gmail.com>
2020-05-05 21:09:57 +0200

    Prettify couted values

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -1,5 +1,8 @@
 #include "utilities.h"
 
+#include <iostream>
+#include <iomanip>
+
 #ifdef LOGGING
 
 void log_dump_payload(const Payload &payload, const std::string header)
@@ -9,9 +12,9 @@ void log_dump_payload(const Payload &payload, const std::string header)
     std::cout << "[" << std::dec << static_cast<int>(payload.size) << "] " << std::hex;
     for (uint8_t i = 0; i < payload.size; ++i)
     {
-        std::cout << std::dec << static_cast<int>(payload.data[i]) << " ";
+        std::cout << std::hex << std::uppercase << std::setfill('0') << std::setw(2) << static_cast<int>(payload.data[i]) << " ";
     }
-    std::cout << std::endl;
+    std::cout << std::dec << std::endl;
 }
 
 void log(std::string in)
--- test/unit/osi_layers/test_unit_base.h
+++ test/unit/osi_layers/test_unit_base.h
@@ -37,9 +37,8 @@ public:
         payloadified_data_with_to_from_crc_ = crc_.append_crc_to_payload(payloadified_data_with_to_from_crc_);
 
         payloadified_data_with_segment_to_from_crc_ = Payload{data_.c_str(), static_cast<uint8_t>(data_.length())};
-        payloadified_data_with_segment_to_from_crc_.data[payloadified_data_with_segment_to_from_crc_.size++] = 1;
-        payloadified_data_with_segment_to_from_crc_.data[payloadified_data_with_segment_to_from_crc_.size++] = 2;
-        payloadified_data_with_segment_to_from_crc_.data[payloadified_data_with_segment_to_from_crc_.size++] = kOwnId;
+        payloadified_data_with_segment_to_from_crc_.data[payloadified_data_with_segment_to_from_crc_.size++] = 0xAB;
+        payloadified_data_with_segment_to_from_crc_.data[payloadified_data_with_segment_to_from_crc_.size++] = 0xCD;
         payloadified_data_with_segment_to_from_crc_.data[payloadified_data_with_segment_to_from_crc_.size++] = kOwnId;
         payloadified_data_with_segment_to_from_crc_.data[payloadified_data_with_segment_to_from_crc_.size++] = kSourceId;
         payloadified_data_with_segment_to_from_crc_ = crc_.append_crc_to_payload(payloadified_data_with_segment_to_from_crc_);
