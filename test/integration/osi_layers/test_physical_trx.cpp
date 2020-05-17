77e888ca13f0c0e6c7696833265a10129e1e56d1
Mihai Galos <mihaigalos@gmail.com>
2020-05-17 09:13:01 +0200

    Improve naming

--- test/integration/osi_layers/test_physical_trx.cpp
+++ test/integration/osi_layers/test_physical_trx.cpp
@@ -19,14 +19,14 @@ public:
     static uint8_t generic_receive_byte()
     {
 
-        return io_data_.at(pos_++);
+        return io_data_.at(pos_in_io_data_++);
     }
 
 protected:
     virtual void SetUp() override
     {
         io_data_ = {};
-        pos_ = {};
+        pos_in_io_data_ = {};
     }
     virtual void TearDown() override {}
 
@@ -35,12 +35,12 @@ protected:
 
     static const std::string send_data_;
     static std::string io_data_;
-    static uint8_t pos_;
+    static uint8_t pos_in_io_data_;
 };
 
 const std::string Fixture::send_data_{"abcd"};
 std::string Fixture::io_data_{};
-uint8_t Fixture::pos_{};
+uint8_t Fixture::pos_in_io_data_{};
 
 TEST_F(Fixture, TRxWorks_WhenTypical)
 {
