fcb7cedfec54881b588e5f8dc2178bc5969d4081
Mihai Galos <mihaigalos@gmail.com>
2020-05-27 22:17:36 +0200

    Make analyzing profiles non-critical

--- scripts/profiling.sh
+++ scripts/profiling.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-bazel analyze-profile --dump=text /tmp/build_profile
-bazel analyze-profile --dump=text /tmp/test_profile
+bazel analyze-profile --dump=text /tmp/build_profile || true
+bazel analyze-profile --dump=text /tmp/test_profile || true
