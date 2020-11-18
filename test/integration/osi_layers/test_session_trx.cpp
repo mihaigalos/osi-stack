696d272c9d5413657acbea1b3530e30f456b13a0
Mihai Galos <mihaigalos@gmail.com>
2020-11-18 10:22:08 +0100

    Use members from super, sync threads

--- test/integration/osi_layers/test_session_trx.cpp
+++ test/integration/osi_layers/test_session_trx.cpp
@@ -2,6 +2,7 @@
 #include <gmock/gmock.h>
 
 #include <memory>
+#include <mutex>
 #include <string>
 #include <array>
 
@@ -11,21 +12,25 @@
 
 using namespace std::chrono_literals;
 bool hasCookie{false};
+std::mutex mutex;
+
 class Fixture : public IntegrationBase
 {
 
 public:
     void SetUp() override
     {
-        index_in_channel_ = 0;
-        channel_.clear();
+        pos_in_io_data_ = 0;
+        io_data_.clear();
         IntegrationBase::SetUp();
     }
 
 protected:
     static void generic_transmit_byte1(const uint8_t payload)
     {
-        channel_ += static_cast<char>(payload);
+        mutex.lock();
+        io_data_ += static_cast<char>(payload);
+        mutex.unlock();
     }
 
     static uint8_t generic_receive_byte1()
@@ -35,58 +40,59 @@ protected:
             std::this_thread::sleep_for(20ms);
         }
 
+        mutex.lock();
         static uint8_t call_count{0};
 
-        char character = channel_[index_in_channel_++];
+        char character = io_data_[pos_in_io_data_++];
 
         ResetChannel(call_count);
+        mutex.unlock();
 
         return static_cast<uint8_t>(character);
     }
 
     static void generic_transmit_byte2(const uint8_t payload)
     {
-        channel_ += static_cast<char>(payload);
+        mutex.lock();
+        io_data_ += static_cast<char>(payload);
         static uint8_t call_count{0};
-        uint8_t total_payload_size{static_cast<uint8_t>(channel_[0])};
+        uint8_t total_payload_size{static_cast<uint8_t>(io_data_[0])};
 
         if (call_count++ == total_payload_size)
         {
             hasCookie = true;
-            index_in_channel_ = 0;
+            pos_in_io_data_ = 0;
         }
+        mutex.unlock();
     }
 
     static uint8_t generic_receive_byte2()
     {
-        char character = channel_[index_in_channel_++];
+        mutex.lock();
+        char character = io_data_[pos_in_io_data_++];
 
         static uint8_t call_count{0};
         ResetChannel(call_count);
+        mutex.unlock();
         return static_cast<uint8_t>(character);
     }
 
     Session<> sut1_{Transport<>{Network<>{kFromId, Datalink<>{Physical{generic_transmit_byte1, generic_receive_byte1}}}}, {"User"}, {"Pass"}, kPort};
     Session<> sut2_{Transport<>{Network<>{kDestinationId, Datalink<>{Physical{generic_transmit_byte2, generic_receive_byte2}}}}, {"User"}, {"Pass"}, kPort};
-    static std::string channel_;
-    static uint8_t index_in_channel_;
 
 private:
     static void ResetChannel(uint8_t &call_count)
     {
-        uint8_t total_payload_size{static_cast<uint8_t>(channel_[0])};
+        uint8_t total_payload_size{static_cast<uint8_t>(io_data_[0])};
 
         if (call_count++ == total_payload_size)
         {
-            channel_.clear();
-            index_in_channel_ = 0;
+            io_data_.clear();
+            pos_in_io_data_ = 0;
             call_count = 0;
         }
     }
 };
-
-std::string Fixture::channel_;
-uint8_t Fixture::index_in_channel_;
 
 TEST_F(Fixture, TRxWorks_WhenTypical)
 {
